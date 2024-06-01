aaf5e6cda30db201f82b10077cf9d95a5ddb8984
Tanmay Agrawal <92915610+TanmayAgrawal2764@users.noreply.github.com>
2024-06-02 02:58:25 +0530

    Delete client directory

--- client/src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-function Footer() {
-  return (
-    <footer>
-      <p>&copy; 2024 Mitzvah Engg.(India) Pvt.. All rights reserved.</p>
-    </footer>
-  );
-}
-export default Footer;
