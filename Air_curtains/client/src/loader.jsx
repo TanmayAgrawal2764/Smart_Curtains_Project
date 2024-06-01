4bde011e80b6f3422c657fd54502a5965b2c3e93
Tanmay Agrawal <92915610+TanmayAgrawal2764@users.noreply.github.com>
2024-06-01 18:56:35 +0530

    Delete Air_curtains directory

--- Air_curtains/client/src/loader.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./index.css";
-function Loader(){
-    return(<div className={"loader-container"}>
-    <img src="./src/curtain.gif" alt="Loading...." className={"loader"}></img>
-    <p className={"loading-text"}>Please wait, page is loading...</p>
-    </div>);
-}
-export default Loader;
